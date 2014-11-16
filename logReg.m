eba8d3a902875da63dda75b0d09232b5af347e0e
CrazyAlan <xiaochuan.chen91@gmail.com>
2014-11-16 13:16:19 -0800

    M

--- logReg.m
+++ logReg.m
@@ -5,8 +5,30 @@ for i=1:TRAIN_SAMPLE
     dataLable = [dataLable;data_train(i).region(:)];
 end
 
-featureIntensity = [ones(size(dataIntensity)) repmat(dataIntensity,1,2)];
-featureIntensity(:,3) = featureIntensity(:,3).^2;
+dataIntensity = [ones(size(dataIntensity)) repmat(dataIntensity,1,2)];
 
-B = mnrfit(featureIntensity,dataLable+1);
+maxIter = 20;
+tol = 0.01;
+w = [0.1 0 0]';
+e_all = [];
+for iter = 1:maxIter
+    y = sigmoid(dataIntensity*w);
+    e = -sum(dataLable.*log(y) + (1-dataLable).*log(1-y));
+    e_all(end+1) = e;
+    
+    Rnn = y.*(1-y);
+    R = diag(Rnn);
+    w_old = w;
+    z = dataIntensity*w_old - (R^-1)*(y-dataLable);
+    w = ((dataIntensity'*R*dataIntensity)^-1)*dataIntensity'*R*z;
+    
+    fprintf('iter %d, negative log-likelihood %.4f, w= \n', iter, e);
+    
+    if iter>1
+        if abs(e-e_all(iter-1))<tol
+          break;
+        end
+    end
+  
+end
 
