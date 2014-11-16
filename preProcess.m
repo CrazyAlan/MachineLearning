ed8b7d794e7cb7d17a93a4b69f4029d5553ce09b
CrazyAlan <xiaochuan.chen91@gmail.com>
2014-11-15 16:41:57 -0800

    Add RGB normalize

--- preProcess.m
+++ preProcess.m
@@ -6,12 +6,14 @@ for i = 1:677
     data_train(i).rgbimage = im2double(data_train(i).rgbimage);
     %meanNormalize
     dataIntensity = [dataIntensity;data_train(i).intensity(:)];
-    
+    dataRGB = [dataRGB; reshape(data_train(i).rgbimage,numel(data_train(i).rgbimage(:,:,1)),3)];
 end
 
 %meanNormalize
 mnIntensity = mean(dataIntensity);
 stdIntensity = std(dataIntensity);
+mnRGB = mean(dataRGB);
+stdRGB = std(dataRGB);
 
 for i = 1:677
     data_train(i).intensity = (data_train(i).intensity - mnIntensity)/stdIntensity;
