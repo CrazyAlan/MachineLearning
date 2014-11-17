233d7a7874ca5d46eec6bf9057386a73480240a9
CrazyAlan <xiaochuan.chen91@gmail.com>
2014-11-16 16:20:57 -0800

    IRLS Regression

--- logReg.m
+++ logReg.m
@@ -11,23 +11,27 @@ dataIntensity = [ones(size(dataIntensity)) repmat(dataIntensity,1,2)];
     
 load dataIntensity.mat;
 load dataLable.mat;
+dataIntensity(:,3) = dataIntensity(:,3).^2;
 maxIter = 100;
 tol = 0.01;
-eta = 0.000001;
-w = [-0.588126826925528 -0.0944626478713500 -0.0944626478713500]';
+eta = 0.0000005;
+w = [1.3 .1 .1]';
 e_all = [];
 for iter = 1:maxIter
    % for i=1:size(dataIntensity,1)
-    y = logsig(dataIntensity*w);
+    y = sigmoid(dataIntensity*w);
     e = -sum(dataLable.*log(y) + (1-dataLable).*log(1-y));
     e_all(end+1) = e;
     
     % Gradient of the error, using Eqn 4.91
-    grad_e = sum(repmat(y - dataLable,[1 size(dataIntensity,2)]) .* dataIntensity, 1);
-    %grad_e = (y(i,1) - dataLable(i,1)).*dataIntensity(i,:);
-    % Update w, *subtracting* a step in the error derivative since we're minimizing
+    Rnn = y.*(1-y);
+    %R = diag(Rnn);
     w_old = w;
-    w = w - eta*grad_e';
+    z = dataIntensity*w_old - (Rnn.^-1).*(y-dataLable);
+    R = repmat(Rnn,1,3);
+    w = (((dataIntensity.*R)'*dataIntensity)^-1)*(dataIntensity.*R)'*z;
+    % Update w, *subtracting* a step in the error derivative since we're minimizing
+    
     
     fprintf('iter %d, negative log-likelihood %.4f, w= \n', iter, e);
     
