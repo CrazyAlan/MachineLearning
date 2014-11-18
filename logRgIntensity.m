1e8a88ead5e337042c2c3a56a73676f87f1a6ca2
CrazyAlan <xiaochuan.chen91@gmail.com>
2014-11-17 17:26:04 -0800

    Preprocessed Data Mean Normalized

---
+++ logRgIntensity.m
@@ -0,0 +1,49 @@
+%{
+dataIntensity = [];
+dataLable = [];
+for i=1:TRAIN_SAMPLE
+    dataIntensity = [dataIntensity;data_train(i).intensity(:)];
+    dataLable = [dataLable;data_train(i).region(:)];
+end
+
+dataIntensity = [ones(size(dataIntensity)) repmat(dataIntensity,1,2)];
+%}
+    
+load dataIntensity.mat;
+load dataLable.mat;
+dataIntensity(:,3) = dataIntensity(:,3).^2;
+dataIntensity = dataIntensity(1:76800,:);
+dataLable = dataLable(1:76800,:);
+maxIter = 100;
+tol = 0.01;
+eta = 0.005;
+w = [0 0 0]';
+e_all = [];
+for iter = 1:maxIter
+   % for i=1:size(dataIntensity,1)
+    y = sigmoid(dataIntensity*w);
+    e = -sum(dataLable.*log(y) + (1-dataLable).*log(1-y));
+    e_all(end+1) = e;
+    
+    % Gradient of the error, using Eqn 4.91
+    Rnn = y.*(1-y);
+    %R = diag(Rnn);
+    w_old = w;
+    z = dataIntensity*w_old - (Rnn.^-1).*(y-dataLable);
+    R = repmat(Rnn,1,3);
+    w = (((dataIntensity.*R)'*dataIntensity)^-1)*(dataIntensity.*R)'*z;
+    % Update w, *subtracting* a step in the error derivative since we're minimizing
+    
+    
+    fprintf('iter %d, negative log-likelihood %.4f, w= \n', iter, e);
+    
+    
+   % end
+    if iter>1
+        if abs(e-e_all(iter-1))<tol
+          break;
+        end
+    end
+  
+end
+
--- preProcess.m
+++ preProcess.m
@@ -4,24 +4,39 @@ TRAIN_SAMPLE = 100;
 load data_sample.mat;
 dataIntensity = [];
 dataRGB = [];
+dataLoc = [];
+dataTxt = [];
+
 for i = 1:TRAIN_SAMPLE
     data_train(i).intensity = im2double(data_train(i).intensity);
-    %data_train(i).rgbimage = im2double(data_train(i).rgbimage);
+    data_train(i).rgbimage = im2double(data_train(i).rgbimage);
     %meanNormalize
     dataIntensity = [dataIntensity;data_train(i).intensity(:)];
-    %dataRGB = [dataRGB; reshape(data_train(i).rgbimage,numel(data_train(i).rgbimage(:,:,1)),3)];
+    dataRGB = [dataRGB; reshape(data_train(i).rgbimage,numel(data_train(i).rgbimage(:,:,1)),3)];
+    dataTxt = [dataTxt; reshape(data_train(i).segament,numel(data_train(i).segament(:,:,1)),3)];
 end
 
 %meanNormalize
 mnIntensity = mean(dataIntensity);
 stdIntensity = std(dataIntensity);
-%mnRGB = mean(dataRGB);
-%stdRGB = std(dataRGB);
+mnRGB = mean(dataRGB);
+stdRGB = std(dataRGB);
+mnTxt = mean(dataTxt);
+stdTxt = std(dataTxt);
 
 for i = 1:TRAIN_SAMPLE
     data_train(i).intensity = (data_train(i).intensity - mnIntensity)/stdIntensity;
-   % data_train(i).rgbimage(:,:,1) = (data_train(i).rgbimage(:,:,1) - mnRGB(1,1))/stdRGB(1,1);
-   % data_train(i).rgbimage(:,:,2) = (data_train(i).rgbimage(:,:,2) - mnRGB(1,2))/stdRGB(1,2);
-   % data_train(i).rgbimage(:,:,3) = (data_train(i).rgbimage(:,:,3) - mnRGB(1,3))/stdRGB(1,3);
+    data_train(i).rgbimage(:,:,1) = (data_train(i).rgbimage(:,:,1) - mnRGB(1,1))/stdRGB(1,1);
+    data_train(i).rgbimage(:,:,2) = (data_train(i).rgbimage(:,:,2) - mnRGB(1,2))/stdRGB(1,2);
+    data_train(i).rgbimage(:,:,3) = (data_train(i).rgbimage(:,:,3) - mnRGB(1,3))/stdRGB(1,3);
+    data_train(i).segament(:,:,1) = (data_train(i).segament(:,:,1) - mnTxt(1,1))/stdTxt(1,1);
+    data_train(i).segament(:,:,2) = (data_train(i).segament(:,:,2) - mnTxt(1,2))/stdTxt(1,2);
+    data_train(i).segament(:,:,3) = (data_train(i).segament(:,:,3) - mnTxt(1,3))/stdTxt(1,3);
 end
+
+
+save dataNorm.mat data_train;
+
+
+
 
